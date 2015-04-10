e59eca9c2b9269598e8b31dcfef9c31728a523c4
310191911 <frank.zhang_2@philips.com>
2015-04-10 13:39:39 +0800

    my demo

---
+++ app/js/app2.js
@@ -0,0 +1,21 @@
+var phonecatApp = angular.module('phonecatApp', [
+  'ngRoute',
+  'phonecatAnimations',
+  'phonecatControllers','phonecatFilters', 'phonecatServices'
+]);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
